51ae7d28cba0a8c073ae4e584dd1f3310f0974e4
togashik <kenta.togashi@kddi-web.com>
2015-11-12 11:01:38 +0900

    ln -fs

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.vim ~/.vim
-ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 git submodule add https://github.com/Shougo/neobundle.vim .vim/bundle/neobundle.vim
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.vim ~/.vim
+ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
