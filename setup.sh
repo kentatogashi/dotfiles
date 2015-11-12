e597b4a4df838bde2820380a1c5c7044844eca2c
togashik <togashik@togashi.k>
2015-11-12 11:51:38 +0900

    add .tmux.conf and fix bug in setup

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git submodule add https://github.com/Shougo/neobundle.vim .vim/bundle/neobundle.vim
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.vim ~/.vim
-ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
